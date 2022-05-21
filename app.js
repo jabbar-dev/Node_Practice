84d0834aadac74f66076ac5cc50a206a365be695
Abdul Jabbar Shah <abduljabbarshah@Abduls-MacBook-Air.local>
2022-05-22 00:42:26 +0500

    Create Routes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const studentRoute = require('./api/routes/student')
+const facultyRoute = require("./api/routes/faculty")
 const app = express();
+
+
+app.use('/student',studentRoute)
+app.use('/faculty',facultyRoute)
 
 app.use((req,res,next)=>{
 
